7a84bbea8741de5ed4c6dfa713f8b9a841d2cae4
Lucas Leme <lucaslssantos99@gmail.com>
2020-08-31 13:49:47 -0300

    PR fix: Add yfinance data

--- hpr.py
+++ hpr.py
@@ -1,14 +1,4 @@
-from alpha_vantage.timeseries import TimeSeries
-
-
-def initialize_alpha_vantage():
-    """
-    Essa função inicializa a api da alpha vantage 
-    """
-
-    key = input('Insira sua chave do alpha vantage: ')
-
-    return TimeSeries(key=key, output_format='pandas')
+import yfinance as yf
 
 
 def get_annual_hpr(ticker, period=252):
@@ -16,12 +6,12 @@ def get_annual_hpr(ticker, period=252):
     Essa função calcula o holding period return anual de junho
     """
 
-    time_series = initialize_alpha_vantage()
+    stock = yf.Ticker(ticker)
 
-    stock_data, _ = time_series.get_daily_adjusted(ticker, outputsize='full')
+    stock_data = stock.history(period="max")
 
-    dividends = stock_data['7. dividend amount']
-    close_prices = stock_data['5. adjusted close']
+    dividends = stock_data['Dividends']
+    close_prices = stock_data['Close']
 
     dividend_period_sum = dividends.copy()
 
